2acd2ddb35f9540334ad13d2cc89c402e3a14bbe
mrhanti <mr.el.hanti@gmail.com>
2014-06-11 23:43:21 +0200

    define new file structure

--- filters/hour/hour.js
+++
@@ -1,5 +0,0 @@
-app.filter('hour', function () {
-    return function (input) {
-        return input.substr(10, 6);
-    }
-});
--- filters/i18n/i18n.js
+++
@@ -1,6 +0,0 @@
-app.filter('i18n',['i18nService', function(i18nService) {
-    return function (input) {
-        return i18nService.getText(input);
-    }
-}]);
-
